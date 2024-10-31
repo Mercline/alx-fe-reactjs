6d0223c8e388cff5d4dda197c54870ba1da08fd9
Mercline Owino <merclineowino9@gmail.com>
2024-10-31 17:43:11 +0300

    my initial commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,21 +1,14 @@
 // src/components/UserProfile.jsx
 import React from 'react';
-import PropTypes from 'prop-types';
 
 function UserProfile(props) {
     return (
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>{props.bio}</p>
+            <h2>{props.name}</h2>       {/* Display user's name */}
+            <p>Age: {props.age}</p>     {/* Display user's age */}
+            <p>{props.bio}</p>          {/* Display user's bio */}
         </div>
     );
 }
-
-UserProfile.propTypes = {
-    name: PropTypes.string.isRequired,
-    age: PropTypes.string.isRequired,
-    bio: PropTypes.string.isRequired, // Ensures bio is included
-};
 
 export default UserProfile;
