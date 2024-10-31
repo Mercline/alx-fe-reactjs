53bc5ce8a1f1cb6280e0418ec019eb4afd885d28
Mercline Owino <merclineowino9@gmail.com>
2024-10-31 17:12:21 +0300

    my initial commit

---
+++ alx-react-app/src/MainContent.jsx
@@ -0,0 +1,12 @@
+// FavoriteCities.jsx
+import React from 'react';
+
+function FavoriteCities() {
+    return (
+        <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default FavoriteCities;
