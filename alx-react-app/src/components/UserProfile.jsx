f9e189023599393c75381183456c53624be0be61
Mercline Owino <merclineowino9@gmail.com>
2024-10-31 17:45:32 +0300

    my initial commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 function UserProfile(props) {
     return (
         <div>
-            <h2>{props.name}</h2>       {/* Display user's name */}
+            <h2>Name: {props.name}</h2>       {/* Display user's name */}
             <p>Age: {props.age}</p>     {/* Display user's age */}
-            <p>{props.bio}</p>          {/* Display user's bio */}
+            <p>Bio: {props.bio}</p>          {/* Display user's bio */}
         </div>
     );
 }
