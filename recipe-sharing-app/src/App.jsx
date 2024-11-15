3853afd0178e73ba1941179393d5296ba973cd86
Mercline Owino <merclineowino9@gmail.com>
2024-11-15 18:06:09 +0300

    Updated a file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import necessary components
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'; // Make sure this component exists
+import AddRecipeForm from './components/AddRecipeForm'; // Add this if you have a form for adding recipes
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router> {/* Wrap your app in Router */}
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes> {/* Define the routes for your app */}
+          <Route path="/" element={<RecipeList />} /> {/* Home route */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* Route for recipe details */}
+          <Route path="/add" element={<AddRecipeForm />} /> {/* Route for adding new recipes */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
