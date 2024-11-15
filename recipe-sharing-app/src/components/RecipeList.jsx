9626bb3eacb5df755f51abda2e2134774485912f
Mercline Owino <merclineowino9@gmail.com>
2024-11-15 18:23:28 +0300

    Made some changes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect } from 'react';
-import { useRecipeStore } from './recipeStore';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useRecipeStore from '../store/recipeStore';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      {filteredRecipes.length > 0 ? (
-        filteredRecipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      ) : (
-        <p>No recipes found.</p>
-      )}
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
+        </div>
+      ))}
     </div>
   );
 };
