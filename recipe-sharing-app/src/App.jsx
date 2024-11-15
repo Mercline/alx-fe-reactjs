459e529f4e7560e79754e20fb572e965f96706dd
Mercline Owino <merclineowino9@gmail.com>
2024-11-15 18:55:30 +0300

    Modified a file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,47 +1,29 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useRecipeStore } from './store/recipeStore';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
+import AddRecipeForm from './components/AddRecipeForm';  // Import the AddRecipeForm
+import RecipeDetail from './components/RecipeDetail';    // Import the RecipeDetail
 
-// RecipeList Component
-const RecipeList = () => {
-  // Get the filtered recipes from the Zustand store
-  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
-  const recipes = useRecipeStore(state => state.recipes);
-
-  useEffect(() => {
-    // If no search term is set, the filteredRecipes will be the same as recipes
-    if (filteredRecipes.length === 0 && recipes.length > 0) {
-      useRecipeStore.getState().filterRecipes(); // Trigger filtering if needed
-    }
-  }, [recipes, filteredRecipes]);
-
-  return (
-    <div>
-      {filteredRecipes.length > 0 ? (
-        filteredRecipes.map((recipe) => (
-          <div key={recipe.id}>
-            <h3>
-              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
-            </h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      ) : (
-        <p>No recipes found.</p>
-      )}
-    </div>
-  );
-};
-
-// Main App Component
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar /> {/* Search bar for filtering recipes */}
-      <RecipeList /> {/* Display the filtered recipe list */}
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar /> {/* Search bar for filtering recipes */}
+
+        <Routes>
+          {/* Route to show all recipes */}
+          <Route path="/" element={<RecipeList />} />
+          
+          {/* Route to show the form to add a new recipe */}
+          <Route path="/add" element={<AddRecipeForm />} />
+          
+          {/* Route to show individual recipe details */}
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
