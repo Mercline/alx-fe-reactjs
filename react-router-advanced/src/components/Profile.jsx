c7a62a1efd26fa7e798e678addf07e466b2a7dc0
Mercline Owino <merclineowino9@gmail.com>
2024-11-22 09:29:47 +0300

    Added some files

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet /> {/* Renders nested routes */}
+    </div>
+  );
+};
+
+export default Profile;
