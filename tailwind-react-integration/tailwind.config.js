3cdf163c5969cec83ac7ac2151c179ea8db930ea
Mercline Owino <merclineowino9@gmail.com>
2024-11-29 09:47:39 +0300

    Added some files

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Specify files for content scanning
+  theme: {
+    extend: {}, // Extend default themes if needed
+  },
+  plugins: [], // Add plugins if required
+};
